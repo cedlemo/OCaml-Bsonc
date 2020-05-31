ea0f56b4d8e28c33dc59740d2e920bab0e17aff0
cedlemo <cedlemo@gmx.com>
2020-05-31 19:26:38 +0200

    Utils: fix style issues

--- lib/utils.ml
+++ lib/utils.ml
@@ -1,6 +1,5 @@
 open Ctypes
 open Foreign
-open Memcpy
 
 (** C String utilities *)
 
@@ -9,18 +8,21 @@ open Memcpy
 let c_strlen =
   foreign "strlen" (ptr char @-> returning int)
 
+let src_off = 0
+let dst_off = 0
+
 let string_to_char_ptr str =
   let open Memcpy in
   let len = String.length str in
-  let dst = allocate_n char (len + 1) in (* Ctypes.allocate* allocate zero filled mem.*)
+  let dst = allocate_n char ~count:(len + 1) in (* Ctypes.allocate* allocate zero filled mem.*)
   let src = Bytes.of_string str in
-  let () = unsafe_memcpy ocaml_bytes pointer src dst len in
+  let () = unsafe_memcpy ocaml_bytes pointer ~src ~dst ~len ~src_off ~dst_off in
   dst
 
-let char_ptr_to_string _ptr =
+let char_ptr_to_string src =
   let open Memcpy in
-  let len = c_strlen _ptr in
-  let buf = Bytes.make len '\000' in
-  let () = unsafe_memcpy pointer ocaml_bytes _ptr buf len in
-  Bytes.to_string buf
+  let len = c_strlen src in
+  let dst = Bytes.make len '\000' in
+  let () = unsafe_memcpy pointer ocaml_bytes ~src ~dst ~len ~src_off ~dst_off in
+  Bytes.to_string dst
 
