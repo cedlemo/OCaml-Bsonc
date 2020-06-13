1043c01d0cfc286e3505b6e42f3d743a8743ebf4
cedlemo <cedlemo@gmx.com>
2020-06-13 19:26:30 +0200

    test/bson_document: refactor

--- test/bson_document.ml
+++ test/bson_document.ml
@@ -18,32 +18,28 @@ let test_append_int32 () =
   let res = Bson.append_int32 document "foo" 1l in
   Alcotest.(check bool) "append ok" true res
 
+let check_json expected = function
+  | None -> Alcotest.fail "json expected"
+  | Some json ->
+    Alcotest.(check string)  "same code" expected json
+
 let test_append_int64 () =
   let document = Bson.create () in
   let res = Bson.append_int64 document "foo" 1L in
   let () = Alcotest.(check bool) "append ok" true res in
-  match Bson.as_json document with
-  | None -> Alcotest.fail "json expected"
-  | Some json ->
-    Alcotest.(check string)  "same code" "{ \"foo\" : 1 }" json
+  check_json "{ \"foo\" : 1 }" @@ Bson.as_json document
 
 let test_append_null () =
   let document = Bson.create () in
   let res = Bson.append_null document "foo" in
   let () = Alcotest.(check bool) "append ok" true res in
-  match Bson.as_json document with
-  | None -> Alcotest.fail "json expected"
-  | Some json ->
-    Alcotest.(check string)  "same code" "{ \"foo\" : null }" json
+  check_json "{ \"foo\" : null }" @@ Bson.as_json document
 
 let test_append_code () =
   let document = Bson.create () in
   let res = Bson.append_code document "SetXtoY" "x = y" in
   let () = Alcotest.(check bool) "append ok" true res in
-  match Bson.as_json document with
-  | None -> Alcotest.fail "json expected"
-  | Some json ->
-    Alcotest.(check string)  "same code" "{ \"SetXtoY\" : { \"$code\" : \"x = y\" } }" json
+  check_json "{ \"SetXtoY\" : { \"$code\" : \"x = y\" } }" @@ Bson.as_json document
 
 let test_append_code_with_scope_with_scope () =
   let document = Bson.create () in
@@ -51,52 +47,36 @@ let test_append_code_with_scope_with_scope () =
   let _ = Bson.append_code scope "identity" "x => x" in
   let res = Bson.append_code_with_scope document "SetXtoY" "x = y" (Some scope) in
   let () = Alcotest.(check bool) "append ok" true res in
-  match Bson.as_json document with
-  | None -> Alcotest.fail "json expected"
-  | Some json ->
-    let expected = "{ \"SetXtoY\" : { \"$code\" : \"x = y\", \"$scope\" : { \"identity\" : { \"$code\" : \"x => x\" } } } }" in
-    Alcotest.(check string) "same code" expected json
+  let expected = "{ \"SetXtoY\" : { \"$code\" : \"x = y\", \"$scope\" : { \"identity\" : { \"$code\" : \"x => x\" } } } }" in
+  check_json expected @@ Bson.as_json document
 
 let test_append_code_with_scope_without_scope () =
   let document = Bson.create () in
   let res = Bson.append_code_with_scope document "SetXtoY" "x = y" None in
   let () = Alcotest.(check bool) "append ok" true res in
-  match Bson.as_json document with
-  | None -> Alcotest.fail "json expected"
-  | Some json ->
-    Alcotest.(check string)  "same code" "{ \"SetXtoY\" : { \"$code\" : \"x = y\" } }" json
+  check_json "{ \"SetXtoY\" : { \"$code\" : \"x = y\" } }" @@ Bson.as_json document
 
 let test_append_date_time () =
   let document = Bson.create () in
   let res = Bson.append_date_time document "foo" 1L in
   let () = Alcotest.(check bool) "append ok" true res in
-  match Bson.as_json document with
-  | None -> Alcotest.fail "json expected"
-  | Some json ->
-    Alcotest.(check string)  "same code" "{ \"foo\" : { \"$date\" : 1 } }" json
+  check_json "{ \"foo\" : { \"$date\" : 1 } }" @@ Bson.as_json document
 
 let test_append_utf8 () =
   let document = Bson.create () in
   let res = Bson.append_utf8 document "foo" "éøö"in
   let () = Alcotest.(check bool) "append ok" true res in
-  match Bson.as_json document with
-  | None -> Alcotest.fail "json expected"
-  | Some json ->
-    Alcotest.(check string)  "same code" "{ \"foo\" : \"éøö\" }" json
+  check_json "{ \"foo\" : \"éøö\" }" @@ Bson.as_json document
 
 let test_as_canonical_extended_json () =
   let document = Bson.create () in
   let _res = Bson.append_int32 document "foo" 1l in
-  match Bson.as_canonical_extended_json document with
-  | None -> Alcotest.fail "json expected"
-  | Some data -> Alcotest.(check string) "same string" "{ \"foo\" : { \"$numberInt\" : \"1\" } }" data
+  check_json "{ \"foo\" : { \"$numberInt\" : \"1\" } }" @@ Bson.as_json document
 
 let test_as_relaxed_extended_json () =
   let document = Bson.create () in
   let _res = Bson.append_int32 document "foo" 1l in
-  match Bson.as_relaxed_extended_json document with
-  | None -> Alcotest.fail "json expected"
-  | Some data -> Alcotest.(check string) "same string" "{ \"foo\" : 1 }" data
+  check_json "{ \"foo\" : 1 }" @@ Bson.as_json document
 
 let tests = [
   test_case "append_bool" `Quick test_append_bool;
